9caa2c34b2d332d2c52be03c2f2120ca7636e80c
profr2 <paulmutua874@gmail.com>
2021-07-26 12:38:55 -0700

    printf

---
+++ printpercent.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+​
+/**
+ * printpercent - prints %
+ * @format: format
+ * @pa: va_list
+ * Return: number of characters printed
+ */
+int printpercent(char *format, va_list pa)
+{
+	(void)format;
+	(void)pa;
+	_putchar('%');
+	return (1);
+}
